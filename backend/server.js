b1a88f733a5ac66e22791ce86849e66e6ef6f967
shruti <shruti@Vedants-MacBook-Air.local>
2026-03-31 11:30:45 +0530

    fix root route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const app = require('./src/app');
 require('./src/config/database')();
 const { generateInterviewReport } = require('./src/services/ai.service');
 
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 
  app.listen(3000,() => {
     console.log(`server is running on port 3000`);
